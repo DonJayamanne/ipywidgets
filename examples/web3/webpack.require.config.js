d2687de6b7771152bd7c9037b9b114b5135f8294
Don Jayamanne <don.jayamanne@yahoo.com>
2019-10-27 15:41:52 -0700

    Eureka

--- examples/web3/webpack.require.config.js
+++ examples/web3/webpack.require.config.js
@@ -46,13 +46,59 @@ var rules = [
     }}
 ];
 
-var publicPath = 'https://unpkg.com/@jupyter-widgets/html-manager@' + version + '/dist/';
+// var publicPath = 'https://unpkg.com/@jupyter-widgets/html-manager@' + version + '/dist/';
 
 module.exports = [
-{// @jupyter-widgets/base
-    entry: './node_modules/@jupyter-widgets/base/lib/index.js',
+{// script that renders widgets using the standard embedding and can only render standard controls
+    entry: './lib/embed.js',
     output: {
-        // library: '@jupyter-widgets/base',
+        filename : 'embed.js',
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath: publicPath,
+    },
+    devtool: 'source-map',
+    module: { rules: rules },
+    mode: 'production'
+},
+{// script that renders widgets using the amd embedding and can render third-party custom widgets
+    entry: './lib/embed-amd-render.js',
+    output: {
+        filename : 'embed-amd-render.js',
+        path: path.resolve(__dirname, 'dist', 'amd'),
+        // publicPath: publicPath,
+    },
+    module: { rules: rules },
+    mode: 'production'
+},
+{// embed library that depends on requirejs, and can load third-party widgets dynamically
+    entry: './lib/libembed-amd.js',
+    output: {
+        library: '@jupyter-widgets/html-manager/dist/libembed-amd',
+        filename : 'libembed-amd.js',
+        path: path.resolve(__dirname, 'dist', 'amd'),
+        // publicPath: publicPath,
+        libraryTarget: 'amd'
+    },
+    module: { rules: rules },
+    mode: 'production'
+},
+{// @jupyter-widgets/html-manager
+    entry: './lib/index.js',
+    output: {
+        library: '@jupyter-widgets/html-manager',
+        filename : 'index.js',
+        path: path.resolve(__dirname, 'dist', 'amd'),
+        // publicPath: publicPath,
+        libraryTarget: 'amd',
+    },
+    module: { rules: rules },
+    externals: ['@jupyter-widgets/base', '@jupyter-widgets/controls'],
+    mode: 'production'
+},
+{// @jupyter-widgets/base
+    entry: '@jupyter-widgets/base/lib/index',
+    output: {
+        library: '@jupyter-widgets/base',
         filename : 'base.js',
         path: path.resolve(__dirname, 'dist', 'amd'),
         // publicPath: publicPath,
@@ -62,9 +108,9 @@ module.exports = [
     mode: 'production'
 },
 {// @jupyter-widgets/controls
-    entry: './node_modules/@jupyter-widgets/controls/lib/index.js',
+    entry: 'node_modules/@jupyter-widgets/controls/lib/index',
     output: {
-        // library: '@jupyter-widgets/controls',
+        library: '@jupyter-widgets/controls',
         filename : 'controls.js',
         path: path.resolve(__dirname, 'dist', 'amd'),
         // publicPath: publicPath,
