97afcc32f58626ab82fa98000ea1dbde701b49b3
Don Jayamanne <don.jayamanne@yahoo.com>
2019-10-28 11:57:36 -0700

    Eureka

--- examples/web3/src/index.ts
+++ examples/web3/src/index.ts
@@ -1,65 +1,80 @@
-import CodeMirror from 'codemirror';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/mode/python/python';
-import 'font-awesome/css/font-awesome.css';
-import {
-    WidgetManager
-} from './manager';
+// import CodeMirror from 'codemirror';
+// import 'codemirror/lib/codemirror.css';
+// import 'codemirror/mode/python/python';
+// import 'font-awesome/css/font-awesome.css';
+import { WidgetManager } from './manager';
 
-import {
-    Kernel, ServerConnection, KernelMessage
-} from '@jupyterlab/services';
+import { Kernel, KernelMessage } from '@jupyterlab/services';
 
-let BASEURL = prompt('Notebook BASEURL', 'http://localhost:8888');
-let WSURL = 'ws:' + BASEURL.split(':').slice(1).join(':');
+// let BASEURL = prompt('Notebook BASEURL', 'http://localhost:8888');
+// let WSURL =
+//   'ws:' +
+//   BASEURL.split(':')
+//     .slice(1)
+//     .join(':');
 
 document.addEventListener('DOMContentLoaded', function(event) {
+  // Connect to the notebook webserver.
+  //   let connectionInfo = ServerConnection.makeSettings({
+  //     baseUrl: BASEURL,
+  //     wsUrl: WSURL
+  //   });
+  if (!(document as any).Kernel) {
+    console.log('Started now getting out of here');
+    return;
+  }
+  console.log('Oops');
+  try {
+    const kernel: Kernel.IKernel = (document as any).getKernel();
+    // JupyterKernel.getSpecs(connectionInfo).then(kernelSpecs => {
+    //     return JupyterKernel.startNew({
+    //         name: kernelSpecs.default,
+    //         serverSettings: connectionInfo
+    //     });
+    // }).then(kernel => {
 
-    // Connect to the notebook webserver.
-    let connectionInfo = ServerConnection.makeSettings({
-        baseUrl: BASEURL,
-        wsUrl: WSURL
-    });
-    Kernel.getSpecs(connectionInfo).then(kernelSpecs => {
-        return Kernel.startNew({
-            name: kernelSpecs.default,
-            serverSettings: connectionInfo
-        });
-    }).then(kernel => {
+    // Create a codemirror instance
+    // let code = require('../widget_code.json').join('\n');
+    // let inputarea = document.getElementsByClassName('inputarea')[0] as HTMLElement;
+    // CodeMirror(inputarea, {
+    //     value: code,
+    //     mode: 'python',
+    //     tabSize: 4,
+    //     showCursorWhenSelecting: true,
+    //     viewportMargin: Infinity,
+    //     readOnly: true
+    // });
+    // Create the widget area and widget manager
+	let widgetarea = document.getElementsByClassName('widgetarea')[0] as HTMLElement;
+	console.log(widgetarea ? 'has widget area' : 'no widget area');
+    let manager = new WidgetManager(kernel, widgetarea);
+	console.log('Created widget manager');
 
-        // Create a codemirror instance
-        let code = require('../widget_code.json').join('\n');
-        let inputarea = document.getElementsByClassName('inputarea')[0] as HTMLElement;
-        CodeMirror(inputarea, {
-            value: code,
-            mode: 'python',
-            tabSize: 4,
-            showCursorWhenSelecting: true,
-            viewportMargin: Infinity,
-            readOnly: true
-        });
-
-        // Create the widget area and widget manager
-        let widgetarea = document.getElementsByClassName('widgetarea')[0] as HTMLElement;
-        let manager = new WidgetManager(kernel, widgetarea);
-
-        // Run backend code to create the widgets.  You could also create the
-        // widgets in the frontend, like the other widget examples demonstrate.
-        let execution = kernel.requestExecute({ code: code });
-        execution.onIOPub = (msg) => {
-            // If we have a display message, display the widget.
-            if (KernelMessage.isDisplayDataMsg(msg)) {
-                let widgetData: any = msg.content.data['application/vnd.jupyter.widget-view+json'];
-                if (widgetData !== undefined && widgetData.version_major === 2) {
-                    let model = manager.get_model(widgetData.model_id);
-                    if (model !== undefined) {
-                        model.then(model => {
-                            manager.display_model(msg, model);
-                        });
-                    }
-                }
-            }
-        };
-    });
+    // Run backend code to create the widgets.  You could also create the
+    // widgets in the frontend, like the other widget examples demonstrate.
+    // let execution = kernel.requestExecute({ code: code });
+    // execution.onIOPub = (msg) => {
+		// const onIOPub: ((msg: KernelMessage.IIOPubMessage) => void | PromiseLike<void>) = (document as any).onIOPub;
+		(document as any).onIOPub = (msg: KernelMessage.IIOPubMessage) => {
+			console.log('Got iopub message');
+      // If we have a display message, display the widget.
+      if (KernelMessage.isDisplayDataMsg(msg)) {
+		  console.log('Yes is display message');
+		  debugger;
+        let widgetData: any = msg.content.data['application/vnd.jupyter.widget-view+json'];
+        if (widgetData !== undefined && widgetData.version_major === 2) {
+          let model = manager.get_model(widgetData.model_id);
+          if (model !== undefined) {
+            model.then(model => {
+              manager.display_model(msg, model);
+            });
+          }
+        }
+      }
+	};
+	console.log('Initialized');
+  } catch (ex) {
+    console.error('Error in ipywidget', ex);
+  }
+  // });
 });
-
--- examples/web3/src/manager.ts
+++ examples/web3/src/manager.ts
@@ -12,10 +12,54 @@ import {
 import './widgets.css';
 import { DOMWidgetView } from '@jupyter-widgets/base';
 
+const cdn = 'https://unpkg.com/';
+
+function moduleNameToCDNUrl(moduleName: string, moduleVersion: string) {
+    let packageName = moduleName;
+    let fileName = 'index'; // default filename
+    // if a '/' is present, like 'foo/bar', packageName is changed to 'foo', and path to 'bar'
+    // We first find the first '/'
+    let index = moduleName.indexOf('/');
+    if ((index != -1) && (moduleName[0] == '@')) {
+        // if we have a namespace, it's a different story
+        // @foo/bar/baz should translate to @foo/bar and baz
+        // so we find the 2nd '/'
+        index = moduleName.indexOf('/', index+1);
+    }
+    if (index != -1) {
+        fileName = moduleName.substr(index+1);
+        packageName = moduleName.substr(0, index);
+    }
+    return `${cdn}${packageName}@${moduleVersion}/dist/${fileName}`;
+}
+
+let requirePromise = function(pkg: string | string[]): Promise<any> {
+    return new Promise((resolve, reject) => {
+        let require = (window as any).requirejs;
+        if (require === undefined) {
+            reject("Requirejs is needed, please ensure it is loaded on the page.");
+        } else {
+            require(pkg, resolve, reject);
+        }
+    });
+}
+
+function requireLoader(moduleName: string, moduleVersion: string) {
+	console.log(`Falling back to ${cdn} for ${moduleName}@${moduleVersion}`);
+	let require = (window as any).requirejs;
+	if (require === undefined) {
+		throw new Error("Requirejs is needed, please ensure it is loaded on the page.");
+	}
+	const conf: {paths: {[key: string]: string}} = {paths: {}};
+	conf.paths[moduleName] = moduleNameToCDNUrl(moduleName, moduleVersion);
+	require.config(conf);
+
+	return requirePromise([`${moduleName}`]);
+}
 export
 class WidgetManager extends HTMLManager {
     constructor(kernel: Kernel.IKernelConnection, el: HTMLElement) {
-        super();
+        super({loader: requireLoader});
         this.kernel = kernel;
         this.el = el;
 
@@ -24,7 +68,27 @@ class WidgetManager extends HTMLManager {
             await this.handle_comm_open(oldComm, msg);
         });
     }
-
+	protected loadClass(className: string, moduleName: string, moduleVersion: string): Promise<any>{
+		debugger;
+		return super.loadClass(className, moduleName, moduleVersion).catch(ex => {
+			console.error('wow', ex);
+			debugger;
+			return requireLoader(moduleName, moduleVersion).catch(ex=>{
+				console.error('wow again', ex);
+				debugger;
+				return Promise.reject(ex);
+			})
+			// return new Promise((resolve, reject)=>{
+			// 	window.require([`https://unpkg.com/${moduleName}@${moduleVersion}/dist/index.js`], resolve, reject);
+			// }).then(function(mod:any) {
+			// 	if (mod[className]) {
+			// 		return mod[className];
+			// 	} else {
+			// 		return Promise.reject(`Class ${className} not found in module ${moduleName}@${moduleVersion}`);
+			// 	}
+			// });
+		})
+	}
     display_view(msg: any, view: DOMWidgetView, options: any) {
         return Promise.resolve(view).then((view) => {
             pWidget.Widget.attach(view.pWidget, this.el);
